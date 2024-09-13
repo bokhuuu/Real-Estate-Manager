2758f8823de42d1725ff2fdd491d74eede899bab
bokhuuu <zurabokhua@gmail.com>
2024-09-13 15:20:17 +0400

    Refactored Header component with styled-components
    - Converted Header template into a functional component
    - Added HeaderContainer and Logo styled-components with specific dimensions

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,25 @@
 import React from "react";
+import styled from "styled-components";
+
+const HeaderContainer = styled.header`
+  width: 1920px;
+  height: 100px;
+  padding: 38px 162px;
+  background: #ffffff;
+  border-bottom: 1px solid #dbdbdb;
+`;
+
+const Logo = styled.img`
+  width: 150px;
+  height: 24px;
+`;
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <HeaderContainer>
+      <Logo src="/src/assets/logo.png" />
+    </HeaderContainer>
+  );
 };
 
 export default Header;
