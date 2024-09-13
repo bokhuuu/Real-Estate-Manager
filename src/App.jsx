a111606923bf22678f777196f79d6e055fd2de10
bokhuuu <zurabokhua@gmail.com>
2024-09-13 12:12:57 +0400

    Initial project setup - Created Vite + React project and deleted unnecessary default files - Organized folder structure for components, pages, and API - Installed dependencies: Axios, React Router DOM, Bootstrap, React-Bootstrap, styled-components - Added react-hook-form, yup, and @hookform/resolvers for form validation - Initialized Git repository and created GitHub repo

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const App = () => {
+  return <div>start project</div>;
+};
+
+export default App;
---
+++ src/pages/AddListing.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const AddListing = () => {
+  return <div></div>;
+};
+
+export default AddListing;
